ba6bec2bde2804d70bf24f189d711c7b2175629f
simkins <mpsim@proton.me>
2026-05-31 23:51:13 -0400

    Build the function to play five rounds and track the score between rounds

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let humanScore = 0;
-let compScore = 0;
-
 function getCompChoice() {
   const n = Math.ceil(Math.random() * 3);
   switch (n) {
@@ -21,27 +18,39 @@ function getHumanChoice() {
   return prompt("rock, paper, or scissors?")
 };
 
-function playRound(humanInput, compChoice) {
-  const humanChoice = humanInput.toLowerCase();
-  let result;
-  if (humanChoice == compChoice) {
-    result = "tie";
-  } else if (
-    (humanChoice == "paper" && compChoice == "rock") 
-    || (humanChoice == "scissors" && compChoice == "paper") 
-    || (humanChoice == "rock" && compChoice == "scissors")
-  ) {
-    result = "win";
-    humanScore++;
-  } else {
-    result = "lose";
-    compScore++;
+function playGame() {
+  
+  let humanScore = 0;
+  let compScore = 0;
+
+  function playRound(humanInput, compChoice) {
+    const humanChoice = humanInput.toLowerCase();
+    let result;
+    if (humanChoice == compChoice) {
+      result = "tie";
+    } else if (
+      (humanChoice == "paper" && compChoice == "rock") 
+      || (humanChoice == "scissors" && compChoice == "paper") 
+      || (humanChoice == "rock" && compChoice == "scissors")
+    ) {
+      result = "win";
+      humanScore++;
+    } else {
+      result = "lose";
+      compScore++;
+    };
+    console.log("you chose " + humanChoice);
+    console.log("the computer chose " + compChoice);
+    console.log("you " + result);
+    console.log("human score: " + humanScore);
+    console.log("computer score: " + compScore);
   };
-  console.log("you chose " + humanChoice);
-  console.log("the computer chose " + compChoice);
-  console.log("you " + result);
-  console.log("human score: " + humanScore);
-  console.log("computer score: " + compScore);
+
+  for (let i = 1; i < 6; i++) {
+    console.log("round " + i);
+    playRound(getHumanChoice(), getCompChoice())
+  };
+
 };
 
-// playRound(getHumanChoice(), getCompChoice())
+playGame();
