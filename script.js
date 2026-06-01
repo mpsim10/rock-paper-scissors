313a77e75a13217cfd28023c7ef106f021dc15f6
simkins <mpsim@proton.me>
2026-05-31 23:44:46 -0400

    Build the function to play a round of the game

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const humanScore = 0;
-const computerScore = 0;
+let humanScore = 0;
+let compScore = 0;
 
-function getComputerChoice() {
+function getCompChoice() {
   const n = Math.ceil(Math.random() * 3);
   switch (n) {
     case 1:
@@ -20,3 +20,28 @@ function getComputerChoice() {
 function getHumanChoice() {
   return prompt("rock, paper, or scissors?")
 };
+
+function playRound(humanInput, compChoice) {
+  const humanChoice = humanInput.toLowerCase();
+  let result;
+  if (humanChoice == compChoice) {
+    result = "tie";
+  } else if (
+    (humanChoice == "paper" && compChoice == "rock") 
+    || (humanChoice == "scissors" && compChoice == "paper") 
+    || (humanChoice == "rock" && compChoice == "scissors")
+  ) {
+    result = "win";
+    humanScore++;
+  } else {
+    result = "lose";
+    compScore++;
+  };
+  console.log("you chose " + humanChoice);
+  console.log("the computer chose " + compChoice);
+  console.log("you " + result);
+  console.log("human score: " + humanScore);
+  console.log("computer score: " + compScore);
+};
+
+// playRound(getHumanChoice(), getCompChoice())
