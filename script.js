7ad86201c2ebb1653911ea377aae509898787315
simkins <mpsim@proton.me>
2026-08-10 17:38:45 -0400

    put option strings in an index and compare their values via index numbers rather than strings

--- script.js
+++ script.js
@@ -1,61 +1,34 @@
-function getCompChoice() {
-  const n = Math.ceil(Math.random() * 3);
-  switch (n) {
-    case 1:
-      return "rock"
-      break;
-    case 2:
-      return "paper"
-      break;
-    case 3:
-      return "scissors"
-    default:
-      break;
-  };
-};
+const options = ["rock", "paper", "scissors"];
 
 function getHumanChoice() {
-  return prompt("rock, paper, or scissors?")
+  return prompt("rock, paper, or scissors?");
 };
 
-function playGame() {
-  
-  let humanScore = 0;
-  let compScore = 0;
-
-  function playRound(humanInput, compChoice) {
-    const humanChoice = humanInput.toLowerCase();
-    if (humanChoice == compChoice) {
-      console.log("you both chose " + humanChoice);
-    } else if (
-      (humanChoice == "paper" && compChoice == "rock") 
-      || (humanChoice == "scissors" && compChoice == "paper") 
-      || (humanChoice == "rock" && compChoice == "scissors")
-    ) {
-      console.log("you win! " + humanChoice + " beats " + compChoice);
-      humanScore++;
-    } else {
-      console.log("you lose :( " + compChoice + " beats " + humanChoice);
-      compScore++;
+function humanChoiceIndex(input) {
+  for (let i = 0; i < options.length; i++) {
+    if (options[i].match(input.toLowerCase())) {
+      return i;
     };
-    console.log("human: " + humanScore + " | computer: " + compScore);
   };
-
-  for (let i = 1; i < 6; i++) {
-    console.log("round " + i);
-    playRound(getHumanChoice(), getCompChoice())
-  };
-
-  console.log("GAME OVER");
-
-  if (humanScore > compScore) {
-    console.log("you win the game!");
-  } else if (humanScore == compScore) {
-    console.log("it's a tie");
-  } else {
-    console.log("you lose the game :(");
-  };
-
+  return undefined;
 };
 
-playGame();
+function computerChoiceIndex() {
+  return Math.floor(Math.random() * 3);
+};
+
+function compareChoices(humanIndex, computerIndex) {
+  return humanIndex - computerIndex;
+};
+
+function testRound() {
+
+  const humanIndex = humanChoiceIndex(getHumanChoice());
+  const computerIndex = computerChoiceIndex();
+
+  console.log(`you chose ${options[humanIndex]}`);
+  console.log(`the computer chose ${options[computerIndex]}`);
+  console.log(`the difference is ${compareChoices(humanIndex, computerIndex)}`);
+};
+
+testRound();
