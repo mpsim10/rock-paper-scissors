f95c6b6e4cb94faec03ef86fd31853c4aa11a61a
simkins <mpsim@proton.me>
2026-08-11 08:28:43 -0400

    reorganize code within round and game functions, add pseudocode as comments for improved readability

--- script.js
+++ script.js
@@ -1,62 +1,58 @@
-const options = ["rock", "paper", "scissors"];
-
-function getHumanChoice() {
-  return prompt("rock, paper, or scissors?");
-};
-
-function humanChoiceIndex(input) {
-  for (let i = 0; i < options.length; i++) {
-    if (options[i].match(input.toLowerCase())) {
-      return i;
-    };
-  };
-  return undefined;
-};
-
-function isValidChoice(choice) {
-  return !isNaN(humanChoiceIndex(choice));
-};
-
-function getComputerChoice() {
-  return Math.floor(Math.random() * 3);
-};
-
-function compareChoices(humanIndex, computerIndex) {
-  const difference = humanIndex - computerIndex;
-  if (difference === 0) {
-    return "tie";
-  } else if (difference === 1 || difference === -2) {
-    return "win";
-  } else {
-    return "lose";
-  };
-};
-
 function testGame() {
   let humanScore = 0;
   let computerScore = 0;
 
-  function testRound() {
+  const options = ["rock", "paper", "scissors"];
 
-    const humanChoice = getHumanChoice();
-    const humanIndex = humanChoiceIndex(humanChoice);
-    const computerIndex = getComputerChoice();
-
-    if (isValidChoice(humanChoice)) {
-      console.log(`you chose ${options[humanIndex]}`);
-      console.log(`the computer chose ${options[computerIndex]}`);
-      console.log(`you ${compareChoices(humanIndex, computerIndex)}`);
-    } else {
-      alert("invalid entry, refresh to start again");
-    }
   
+  function playRound() {
+    
+    // determine the index of the option corresponding to the human's choice
+    function getHumanChoice() {
+      const input = prompt("rock, paper, or scissors?");
+      for (let i = 0; i < options.length; i++) {
+        if (options[i].match(input.toLowerCase())) {
+          return i;
+        };
+      };
+      return undefined;
+    };
+
+    const human = getHumanChoice();
+    // generate a random index in the options array to assign as the computer's choice
+    const computer = Math.floor(Math.random() * 3);
+
+    // compare index values to determine the winner and increase points
+    function resolveRound() {
+      const difference = human - computer;
+      if (difference === 0) {
+        return;
+      } else if (difference === 1 || -2) {
+        humanScore++;
+      } else {
+        computerScore++;
+      };
+    };
+    
+    function displayResults() {
+
+      console.log(`you chose ${options[human]}`);
+      console.log(`the computer chose ${options[computer]}`);
+      console.log(`human ${humanScore} | computer ${computerScore}`);
+
+    }
+
+    // check if the input is valid before trying to resolve the round
+    if (!isNaN(human)) {
+      resolveRound(human, computer);
+      displayResults();
+    } else {
+      alert("invalid entry – you lose! refresh to start again");
+    };
+
   };
 
-  testRound();
-  testRound();
-  testRound();
-  testRound();
-  testRound();
+  playRound();
 
 };
 
