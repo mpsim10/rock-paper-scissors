cfefaabe922ac428c490676d65125cc9d8bd4671
simkins <mpsim@proton.me>
2026-08-10 20:10:57 -0400

    validate human input and determine winner by comparing array indices

--- script.js
+++ script.js
@@ -13,22 +13,37 @@ function humanChoiceIndex(input) {
   return undefined;
 };
 
+function isValidChoice(choice) {
+  return !isNaN(humanChoiceIndex(choice));
+};
+
 function computerChoiceIndex() {
   return Math.floor(Math.random() * 3);
 };
 
 function compareChoices(humanIndex, computerIndex) {
-  return humanIndex - computerIndex;
+  const difference = humanIndex - computerIndex;
+  if (difference === 0) {
+    return "tie"
+  } else if (difference === 1 || difference === -2) {
+    return "win"
+  } else {
+    return "lose"
+  };
 };
+
+
 
 function testRound() {
 
-  const humanIndex = humanChoiceIndex(getHumanChoice());
+  let humanChoice = getHumanChoice()
+  const humanIndex = humanChoiceIndex(humanChoice);
   const computerIndex = computerChoiceIndex();
 
+  console.log(isValidChoice(humanChoice));
   console.log(`you chose ${options[humanIndex]}`);
   console.log(`the computer chose ${options[computerIndex]}`);
-  console.log(`the difference is ${compareChoices(humanIndex, computerIndex)}`);
+  console.log(`you ${compareChoices(humanIndex, computerIndex)}`);
 };
 
 testRound();
