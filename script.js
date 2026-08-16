d5ddc0577fcc3197ba2a60240ee1a7b9b71972da
simkins <mpsim@proton.me>
2026-08-16 11:21:49 -0400

    initialize ui outside of game function, adjust position, remove game resolution to troubleshoot score increment issue

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 const choices = ["rock", "paper", "scissors"];
 
+for (let i = 0; i < choices.length; i++) {
+  const choiceButton = document.createElement("button");
+  choiceButton.setAttribute("id", i);
+  choiceButton.classList.add("choice-button");
+  choiceButton.textContent = choices[i];
+  document.body.appendChild(choiceButton);
+};
+
+const messageDisplay = document.createElement("div");
+messageDisplay.id = "message-display";
+messageDisplay.textContent = "make your first choice to begin";
+document.body.appendChild(messageDisplay);
+
+const scoreDisplay = document.createElement("div");
+scoreDisplay.id = "score-display";
+document.body.appendChild(scoreDisplay);
+
+function updateDisplay(display, human, computer) {
+  display.textContent = `human: ${human} | computer: ${computer}`;
+};
+
 function playGame() {
+
   let humanScore = 0;
   let computerScore = 0;
   let resultMessage;
-
-  const messageDisplay = document.createElement("div");
-  messageDisplay.id = "message-display";
-  messageDisplay.textContent = "make your first choice to begin";
-  document.body.appendChild(messageDisplay);
-
-  const scoreDisplay = document.createElement("div");
-  scoreDisplay.id = "score-display";
-  document.body.appendChild(scoreDisplay);
-
-  function updateDisplay(display, human, computer) {
-    display.textContent = `human: ${human} | computer: ${computer}`;
-  };
 
   function playRound(playerSelection) {
     const human = Number(playerSelection);
@@ -31,33 +40,6 @@ function playGame() {
     };
     updateDisplay(messageDisplay, choices[human], choices[computer]);
     updateDisplay(scoreDisplay, humanScore, computerScore);
-  };
-
-  function resolveGame() {
-    const difference = humanScore - computerScore;
-    if (difference === 0) {
-      resultMessage = "it was a tie :|"
-    } else if (difference > 0) {
-      resultMessage = "you won! :D"
-    } else {
-      resultMessage = "you lost. :("
-    };
-  };
-
-  function displayResults() {
-    alert(`
-    final score: human ${humanScore}, computer ${computerScore}
-    ${resultMessage}
-    refresh to play again
-    `);
-  };
-  
-  for (let i = 0; i < choices.length; i++) {
-    const choiceButton = document.createElement("button");
-    choiceButton.setAttribute("id", i);
-    choiceButton.classList.add("choice-button");
-    choiceButton.textContent = choices[i];
-    document.body.appendChild(choiceButton);
   };
   
   const choiceButtons = document.querySelectorAll(".choice-button");
